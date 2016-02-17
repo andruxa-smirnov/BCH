e93a219453617626990fafbcf0d56136f84a39aa
Tmg82 <tmg82@rambler.ru>
2016-02-17 17:04:21 +0400

    1. BCH emplementation project

---
+++ BCH/BCH.cpp
@@ -0,0 +1,13 @@
+// BCH.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "bch3.h"
+#include <iostream>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	bch_main();		
+	return 0;
+}
+
---
+++ BCH/bch3.h
@@ -0,0 +1,6 @@
+#ifndef __BCH3_H__
+#define __BCH3_H__
+
+void bch_main();
+
+#endif 
